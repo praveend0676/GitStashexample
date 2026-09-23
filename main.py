4c6f656d8b5796d3e0bf89457f5cf628a6ae840a
praveend0676 <praveendevisetti0676@gmail.com>
2026-09-23 20:48:34 +0530

    Resolve merge conflicts in main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 def welcome():
     print("Demo of Git Stash example")
     print("Urgent work or production fix")
-<<<<<<< HEAD
     print("making some progress")
-=======
     print("Urgent production fix applied")
 
 welcome()
->>>>>>> urgent_fix
